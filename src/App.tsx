35de8688b6b9ed512f304310d92df0317bbb6c2b
Skillygonzales <eandrei271@gmail.com>
2023-05-31 12:22:50 +0200

    Adding props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import Message from './Message';
-import ListGroup from './components/ListGroup';
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup></ListGroup></div>;
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
